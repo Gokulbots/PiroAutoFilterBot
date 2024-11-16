1e5589237fec3f4fef49346563cdaddd1708b2a4
Gokulbots <botsdemo9@gmail.com>
2024-11-16 09:45:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,7 +51,7 @@ SUPPORT_CHAT_ID = -1002223220917
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://DS:DS@cluster0.rc3ccyx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
